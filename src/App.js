92d4b0609416bf7970c47e45da7bbe2590ed0ce1
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-10-12 20:04:42 -0500

    Adds Sentry to package and initializes analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactGA from "react-ga";
 
 function App() {
+  initializeReactGA();
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +23,11 @@ function App() {
       </header>
     </div>
   );
+}
+
+function initializeReactGA() {
+  ReactGA.initialize("UA-104321386-1");
+  ReactGA.pageview("/homepage");
 }
 
 export default App;
