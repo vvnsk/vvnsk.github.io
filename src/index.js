3440736a5bea31978b820c29a267ca09e8813c89
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-10-13 17:08:45 -0500

    New template init

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import * as Sentry from "@sentry/browser";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
