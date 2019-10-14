318d96a34b35e64bfc278c1bc34f3ff9cb0b652d
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-10-13 21:13:37 -0500

    Removed contact and added blink animation to typed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
 import Resume from "./Components/Resume";
-import Contact from "./Components/Contact";
 import Certifications from "./Components/Certifications";
 import Portfolio from "./Components/Portfolio";
 
@@ -48,7 +47,6 @@ class App extends Component {
         <Resume data={this.state.resumeData.resume} />
         <Portfolio data={this.state.resumeData.portfolio} />
         <Certifications data={this.state.resumeData.certifications} />
-        <Contact data={this.state.resumeData.main} />
         <Footer data={this.state.resumeData.main} />
       </div>
     );
