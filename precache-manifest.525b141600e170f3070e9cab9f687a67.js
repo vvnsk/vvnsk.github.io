c505b6a055f9e58ba727a9eacbe5106ec5d89418
Saikrishna <vinnakotavnsaikrishna@gmail.com>
2019-10-17 21:59:09 -0500

    Updates

---
+++ precache-manifest.525b141600e170f3070e9cab9f687a67.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49194957bf7dfdfc9bc11e954fbef841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dee5d25ced7df4dfb9be",
+    "url": "/static/js/2.15cc9256.chunk.js"
+  },
+  {
+    "revision": "be6a1e1e09d70a1800bc",
+    "url": "/static/js/main.f54fd824.chunk.js"
+  },
+  {
+    "revision": "add6d1cb66cb3700105e",
+    "url": "/static/js/runtime-main.fe102257.js"
+  }
+]);
