0d9f30147d93ca57e8d83227955ce5152c1e26f6
Vinnakota Saikrishna <vinnakotavnsaikrishna@gmail.com>
2022-05-03 19:47:18 -0500

    Contact Form init

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,11 +1,26 @@
 import HomeLink from "../../components/HomeLink/HomeLink";
+import "./Contact.css"
 
 function Contact() {
   return (
-    <>
+    <div className="page-wrapper contact-wrapper">
       <HomeLink />
       <h2>Contact</h2>
-    </>
+      <form className="contact-form" name="contact" method="POST" data-netlify="true">
+        <p>
+          <input type="text" name="name" placeholder="Name" />
+        </p>
+        <p>
+          <input type="email" name="email" placeholder="Email" />
+        </p>
+        <p>
+          <textarea name="message" placeholder="Hello Sai..."></textarea>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
   );
 }
 
